e848f35f8d8601c24a93d39c7c015fcad1b76ebe
jevus09 <jevus09@gmail.com>
2024-06-15 12:54:33 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      height: {
+        '128': '66vh',
+        '100': '12rem',
+        '104': '75vh',
+        '112': '32rem',
+      },
+      width: {
+        '128': '66vh',
+        '104': '90vw',
+        '108': '100vw',
+        '100': '40rem',
+      }
+    },
+  },
+  plugins: [],
+}
+
